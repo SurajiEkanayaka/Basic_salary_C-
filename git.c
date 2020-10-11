0e7ca396eb9868bd3ed37f187b4d01ed79e9ff68
SurajiEkanayaka <suraji24ekanayaka24@gmail.com>
2020-10-11 23:58:24 +0530

    updated

--- git.c
+++ git.c
@@ -1,46 +1,25 @@
-#include <stdio.h> 
-#include <stdlib.h> 
- 
-int main() 
-{ 
-   char s_name[20],area; 
- 
-   float b_salary,sal; 
-   int serv,sales; 
-   printf("Enter your name : "); 
-   scanf("%s",&s_name); 
-   printf("Enter the area : "); 
-   scanf("%s",&area); 
-   printf("Enter your basic salary : "); 
-   scanf("%f",&b_salary); 
-   printf("Enter your number of years in service : "); 
-   scanf("%d",&serv); 
-   printf("Monthly sales : "); 
-   scanf("%d",&sales); 
-   sal=b_salary; 
-   if (serv>5) 
-   { 
-      sal=sal+(sal*0.1); 
-   } 
-   if (area=='C') 
-   { 
-       sal=sal+2500; 
-   }
- 
-   if (sales<=25000) 
-   { 
-       sal=sal+(sales*0.1); 
-   } 
-   else if (sales<50000) 
-   { 
-       sal=sal+(sales*0.12); 
+#include <stdio.h>
+#include <stdlib.h>
 
-   } 
-   else if (sales>=50000) 
-   { 
-       sal=sal+(sales*0.15); 
-   } 
-   printf("Name : %s \n",s_name); 
-   printf("Your gross monthly remuneration is %f ",sal); 
-    return 0; 
+int main()
+{
+   char s_name[20],area;
+
+   float b_salary,sal;
+   int serv,sales;
+   printf("Enter your name : ");
+   scanf("%s",&s_name);
+   printf("Enter the area : ");
+  //N
+  //A
+
+   if (sales<=25000)
+   {
+       sal=sal+(sales*0.1);
+   }
+  //T
+  //I
+   printf("Name : %s \n",s_name);
+   printf("Your gross monthly remuneration is %f ",sal);
+    return 0;
 }
